d83d455871171e295b3093c4e5d1a2f43eb4b677
Steve Mayhew <mayhew.steve@gmail.com>
2015-12-18 11:07:31 -0800

    Add build that updates local mediaelement

--- build/build_copy_to_mediaelementjs.sh
+++ build/build_copy_to_mediaelementjs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$FLEXPATH" ]; then
-  export FLEXPATH=/Users/smayhew/flex_sdk_4
+  export FLEXPATH=~/flex_sdk_4
 fi
 ./build.sh
-cp ../bin/debug/flashls.swc ../../mediaelement/src/flash/flashls.swc 
+cp ../bin/debug/flashlsOSMF.swc ../../mediaelement/src/flash/
 
